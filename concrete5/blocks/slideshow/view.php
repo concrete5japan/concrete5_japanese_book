13d825d27c73710fe3783aa586a9001eaddc668b
hissy <hishikawa@concrete5.co.jp>
2013-01-02 21:56:08 +0900

    bug fix

--- concrete5/blocks/slideshow/view.php
+++ concrete5/blocks/slideshow/view.php
@@ -1,13 +1,10 @@
 <?php	 defined('C5_EXECUTE') or die("Access Denied."); ?>
 <div id="orbit">
 	<?php	 
-	$notFirst=1;
 	foreach($images as $imgInfo) {
 		$f = File::getByID($imgInfo['fID']);
 		$fp = new Permissions($f);
 		if ($fp->canViewFile()) {
-			if(!$notFirst) echo ',';
-			$notFirst=0
 			?>
 			<div>
 				<?php if($imgInfo['url']): ?><a href="<?php	echo $imgInfo['url']?>"><?php endif; ?>
