ce3b49f1da87d2a5f3a3e15a946f9b8cfd81868b
hissy <hishikawa@concrete5.co.jp>
2013-01-07 04:00:22 +0900

    customize "Small" template for Google Map block

--- concrete5/blocks/google_map/templates/small/view.php
+++ concrete5/blocks/google_map/templates/small/view.php
@@ -2,10 +2,10 @@
 
 $c = Page::getCurrentPage();
 if ($c->isEditMode()) { ?>
-	<div class="ccm-edit-mode-disabled-item" style="height: 400px">
+	<div class="ccm-edit-mode-disabled-item" style="height: 250px">
 		<div style="padding: 80px 0px 0px 0px"><?php	echo t('Google Map disabled in edit mode.')?></div>
 	</div>
 <?php	 } else { ?>	
 	<?php	 if( strlen($title)>0){ ?><h3><?php	echo $title?></h3><?php	 } ?>
-	<div id="googleMapCanvas<?php	echo $bID?>" class="googleMapCanvas"></div>	
+	<div id="googleMapCanvas<?php	echo $bID?>" class="googleMapCanvasSmall panel"></div>	
 <?php	 } ?>
