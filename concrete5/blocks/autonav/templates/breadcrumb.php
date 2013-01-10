a5b764e2d0ff40d5a08b9eb95dbfa3184d0c2cdd
hissy <hishikawa@concrete5.co.jp>
2013-01-10 21:55:34 +0900

    Override Breadcrumb template for autonav block

---
+++ concrete5/blocks/autonav/templates/breadcrumb.php
@@ -0,0 +1,14 @@
+<?php	 defined('C5_EXECUTE') or die(_("Access Denied."));
+$navItems = $controller->getNavItems();
+
+foreach ($navItems as $ni) {
+	if (!$ni->isFirst) {
+		echo ' <span class="ccm-autonav-breadcrumb-sep">&gt;</span> ';
+	}
+	
+	if ($ni->isCurrent) {
+		echo $ni->name;
+	} else {
+		echo '<a href="' . $ni->url . '" target="' . $ni->target . '">' . $ni->name . '</a>';
+	}
+}
