52a732d0ae5fa6aa27b2c31b5a5fca69564714d2
hissy <hishikawa@concrete5.co.jp>
2013-10-03 23:29:07 +0900

    show errors in view.php

--- concrete5/themes/olive_sample/view.php
+++ concrete5/themes/olive_sample/view.php
@@ -5,6 +5,7 @@ $this->inc('elements/header.php'); ?>
 	<div class="row">
 		<section id="main" class="twelve columns" role="main">
 		
+			<?php  Loader::element('system_errors', array('error' => $error)); ?>
 			<?php	 print $innerContent; ?>
 			
 		</section>
